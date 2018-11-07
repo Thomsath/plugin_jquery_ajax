91292cb4302e7ae873e9a99db49166009d013d6e
amace <amace@compagnie-hyperactive.com>
2018-11-07 17:32:35 +0100

    working on refactoring code

--- pluginvide.js
+++ pluginvide.js
@@ -1,45 +1,51 @@
-	// on déclare notre code dans une fonction anonyme
-// que l'on appelle aussitôt avec le paramètre jQuery
-// alors récupéré dans la variable $ permettant ainsi
-// d'utiliser $() plutôt que jQuery() (plus pratique...)
 (function($) {
-
 	// Déclaration du plugin
 	$.fn.pluginVide = function(parameters) {
 
-		// Déclaration des paramètres par défaut
-		// puis fusion de ces dernier aves les
-		// paramètres passés par l'utilisateur
-		var defaultValues = {
+		const defaultValues = {
 			ajaxTriggers: $('a'),
-			loader: $('.gooey'),
-			content: $('.ajaxContent')
+			loader: 'loader.html',
+			content: $('body')
 		};
+		// fusion des paramètres par défaut ainsi que ceux passés par l'utilisateur
 		var parameters = $.extend(defaultValues, parameters);
 
 		parameters.ajaxTriggers.click(function(event) {
-			parameters.content.hide();
-		  	const newContent = $(this).attr('href') + " .ajaxContent";
-		  	event.preventDefault();
-		  	history.pushState(newContent, null);
-		  	parameters.content.load(newContent, function() {
-  				parameters.loader.hide(200);
-				parameters.content.show(300);
-			});
+            event.preventDefault();
+            hideOldContent(parameters.content);
+            showLoader(parameters.loader, parameters.content);
+            showNewContent($(this).attr('href'), parameters.content, parameters.loader);
+        });
 
-		  	parameters.loader.show(500);
+        window.addEventListener('popstate', function(e) {
+            const lastContent = e.state;
+            newContent.load(lastContent);
+        });
 
-		});
-		
-		// logger la cible
+		function hideOldContent(oldContent) {
+            oldContent.hide(0);
+        }
+        function showLoader(loader, destination) {
+            destination.load(loader);
+
+        }
+        function hideLoader(loader) {
+			$('body').hide(0);
+        }
+        function showNewContent(newContent, destination, loader) {
+            history.pushState(newContent, null);
+            destination.load(newContent, function() {
+                hideLoader(loader);
+                $('body').show(300);
+            });
+
+        }
+        // logger la cible
 		console.log(this);
+
 		// logger les paramètres
 		console.log(parameters);
 
-		window.addEventListener('popstate', function(e) {
-		  	const lastContent = e.state;
-		  	newContent.load(lastContent);
-		});
 		// on renvoie la cible pour permettre le chainage de
 		// notre plugin avec d'autres fonctions ou plugins jQuery
 		return this;
