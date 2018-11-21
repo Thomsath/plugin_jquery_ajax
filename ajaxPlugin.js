bc623fc85881dec2b62f65e802e503b669f45fc8
alexandre-mace <contact.alexandremace@gmail.com>
2018-11-21 09:20:33 +0100

    working on ajax

---
+++ ajaxPlugin.js
@@ -0,0 +1,66 @@
+(function($) {
+	// Déclaration du plugin
+	$.fn.ajaxPlugin = function(options) {
+
+		const defaultParameters = {
+			content: $('body'),
+            ajaxTriggers: $('a'),
+            loaderParameters: {
+                url: 'https://loading.io/spinners/coffee/index.coffee-cup-drink-loader.gif',
+                css: {
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    display: "none"
+                }
+            }
+		};
+		// fusion des paramètres par défaut ainsi que ceux passés par l'utilisateur
+		var parameters = $.extend(defaultParameters, options);
+
+        generateLoader(parameters.loaderParameters);
+        $(document).ajaxStart(function(){
+            showLoader();
+        });
+        $(document).ajaxError(function(){
+            alert('Something went wrong');
+        });  
+        $(document).ajaxSuccess(function(){
+            hideLoader();
+        });
+        parameters.ajaxTriggers.click(function(event) {
+            event.preventDefault();
+            showNewContent($(this).attr('href'), parameters.content);
+        });
+
+        window.onpopstate =  function(event) {
+            const lastContent = event.state;
+            event.preventDefault();
+            $('body').load(lastContent);
+        };
+
+        function generateLoader(loaderParameters) {
+            $('body').append(`<div id="loader"><img src="${loaderParameters.url}" /></div>`);
+            $('#loader').css(loaderParameters.css);
+        }
+
+        function showLoader() {
+            $("#loader").css("display", "block");
+        }
+
+        function hideLoader() {
+            $("#loader").css("display", "none");
+        }
+
+        function showNewContent(newContent, destination) {
+            destination.load(newContent).fadeIn();
+            history.pushState(newContent, null);
+        }
+		// on renvoie la cible pour permettre le chainage de
+		// notre plugin avec d'autres fonctions ou plugins jQuery
+		return this;
+	};
+
+
+})(jQuery);
