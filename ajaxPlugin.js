49120b2797a24c64877cbe793b03eec93f236b65
alexandre-mace <contact.alexandremace@gmail.com>
2018-11-29 01:50:21 +0100

    updated plugin

--- ajaxPlugin.js
+++ ajaxPlugin.js
@@ -1,13 +1,12 @@
 (function($) {
-	// Déclaration du plugin
 	$.fn.ajaxPlugin = function(options) {
-
-		const defaultParameters = {
-			content: $('body'),
-            ajaxTriggers: $('a'),
-            loaderParameters: {
-                url: 'https://loading.io/spinners/coffee/index.coffee-cup-drink-loader.gif',
-                css: {
+	    // PARAMETERS
+		const parameters = $.extend({
+			destination: $("body"),
+            ajaxTriggers: $("a"),
+            loader: {
+                url: "https://loading.io/spinners/coffee/index.coffee-cup-drink-loader.gif",
+                style: {
                     position: "absolute",
                     top: "50%",
                     left: "50%",
@@ -15,54 +14,95 @@
                     visibility: "hidden"
                 }
             }
-		};
-		// fusion des paramètres par défaut ainsi que ceux passés par l'utilisateur
-		var parameters = $.extend(defaultParameters, options);
+		}, options);
 
-        generateLoader(parameters.loaderParameters);
-        $(document).ajaxStart(function(){
-            showLoader();
-        });
-        $(document).ajaxError(function(){
-            alert('Something went wrong');
-        });  
-        $(document).ajaxSuccess(function(){
-            hideLoader();
-        });
+		// OBSERVERS
         parameters.ajaxTriggers.click(function(event) {
             event.preventDefault();
-            showNewContent($(this).attr('href'), parameters.content);
+            AjaxContent.newContent = $(this).attr("href");
+            AjaxContent.destination = parameters.destination;
+            AjaxContent.load(function(response, status, xhr) {
+                if ( status == "success" ) {
+                    Browser.updateHistory(AjaxContent.newContent, "");
+                    Browser.sluggedTitle = slugify(removeExtension(AjaxContent.newContent));
+                    Browser.updateTitle();
+                }
+            });
         });
-
+        $(document).ajaxStart(function(){
+            Loader.append(parameters.loader);
+            Loader.show();
+            AjaxContent.hide();
+        });
+        $(document).ajaxError(function(){
+            alert("Something went wrong, please contact the site owner");
+        });
+        $(document).ajaxSuccess(function(){
+            AjaxContent.show();
+            Loader.hide();
+        });
         window.onpopstate =  function(event) {
-            const lastContent = event.state;
             event.preventDefault();
-            $('body').load(lastContent);
+            if (event.state) {
+                parameters.destination.load(event.state);
+            }
         };
 
-        function generateLoader(loaderParameters) {
-            $('body').append(`<div id="loader"><img src="${loaderParameters.url}" /></div>`);
-            $('#loader').css(loaderParameters.css);
-        }
-
-        function showLoader() {
-            $('body').css("visibility", "hidden");
-            $("#loader").css("visibility", "visible");
-        }
-
-        function hideLoader() {
-            $("#loader").css("visibility", "hidden");
-            $('body').not('#loader').css("visibility", "visible");
-        }
-
-        function showNewContent(newContent, destination) {
-            destination.load(newContent);
-            history.pushState(newContent, null);
-        }
-		// on renvoie la cible pour permettre le chainage de
-		// notre plugin avec d'autres fonctions ou plugins jQuery
 		return this;
 	};
 
+    // ENTITIES
+    const Browser = {
+        sluggedTitle: "",
+        updateHistory: function (newContent, title) {
+            history.pushState(newContent, title);
+        },
+        updateTitle: function () {
+            document.title = this.sluggedTitle.toUpperCase();
+        }
+    };
+	const Loader = {
+	    append: function (parameters) {
+            $("body").append(`<div id="loader"><img src="${parameters.url}" /></div>`);
+            this.setStyle(parameters.style);
+        },
+        show: function () {
+            $("#loader").css("visibility", "visible");
+        },
+        hide: function () {
+            $("#loader").css("visibility", "hidden");
+        },
+        setStyle: function (style) {
+            $("#loader").css(style);
+        }
+    };
+    const AjaxContent = {
+        newContent: "",
+        destination: "",
+        load: function (callback = null) {
+            this.destination.load(this.newContent, callback);
+        },
+	    show: function () {
+            $("body").css("visibility", "visible");
+        },
+        hide: function () {
+            $("body").css("visibility", "hidden");
+        }
+    };
+
+    // SERVICES
+    function slugify(text)
+    {
+        return text.toString().toLowerCase()
+            .replace(/\s+/g, "-")           // Replace spaces with -
+            .replace(/[^\w\-]+/g, "")       // Remove all non-word chars
+            .replace(/\-\-+/g, "-")         // Replace multiple - with single -
+            .replace(/^-+/, "")             // Trim - from start of text
+            .replace(/-+$/, "");            // Trim - from end of text
+    }
+
+    function removeExtension(text) {
+        return text.substring(0, text.lastIndexOf("."));
+    }
 
 })(jQuery);
